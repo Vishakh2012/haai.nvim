28585ad90409b82a80898ff4515358b1c1e6c648
vishakh prakash <vishakhpro2002@gmail.com>
2026-08-14 17:23:32 +0530

    make changes

--- plugin/haai.lua
+++ plugin/haai.lua
@@ -1,11 +1,19 @@
--- plugin/haai.lua
-
 if vim.g.loaded_haai then
   return
 end
 
 vim.g.loaded_haai = true
 
+local haai = require("haai")
+
 vim.api.nvim_create_user_command("Haai", function()
-  require("haai").open()
-end, {})
+  haai.open()
+end, {
+  desc = "Open Haai",
+})
+
+vim.keymap.set({ "n", "v" }, "<leader>h", function()
+  haai.open()
+end, {
+  desc = "Open Haai",
+})
