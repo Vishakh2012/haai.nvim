a1e57941e35a35563d552a51be637928626da199
vishakh prakash <vishakhpro2002@gmail.com>
2026-08-14 17:30:32 +0530

    add some changes

--- lua/haai/init.lua
+++ lua/haai/init.lua
@@ -8,9 +8,17 @@ function M.ask(prompt, selection)
     return require("haai.config").ask(prompt, selection)
 end
 
-function M.open(selection)
-    print("HAAI: M.open()")
-    print("HAAI: selection =", vim.inspect(selection))
+function M.open()
+    local selection = nil
+
+    local mode = vim.fn.mode()
+
+    if mode == "v" or mode == "V" or mode == "\22" then
+        selection = require("haai.ui.selection").get()
+
+        print("HAAI selection:", vim.inspect(selection))
+    end
+
     require("haai.ui.float").open(selection)
 end
 
